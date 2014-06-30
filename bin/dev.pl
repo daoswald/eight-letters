9c07a9effa9544e5af7a2545a6e4b4244f2614ec
David Oswald <daoswald@gmail.com>
2014-06-30 12:40:51 -0600

    Refined bin/dev.pl slightly.

--- bin/dev.pl
+++ bin/dev.pl
@@ -1,19 +1,18 @@
 #!/usr/bin/env perl
+
 my $t;
 BEGIN{
   use Time::HiRes;
   $t = [Time::HiRes::gettimeofday()];
 }
+
 use FindBin;
 use lib "$FindBin::Bin/../lib";
 use EightLetters;
 
 use constant TEST_DICT => "$FindBin::Bin/../lib/dict/test_dict.txt";
 
-my $el = EightLetters->new;
-my( $letters, $count ) = ( $el->letters, $el->count );
-
-print "$letters spells $count words.\n";
+print $_->letters, " spells ", $_->count, " words.\n" for EightLetters->new;
 
 END{
   printf "Duration: %.03f seconds.\n", Time::HiRes::tv_interval($t);
