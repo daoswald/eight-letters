f00534ccdf56191d5f8b584780347d59383eae0b
David Oswald <david.oswald@gmail.com>
2017-11-25 13:19:45 -0700

    Breakthrough on Parallel::ForkManager optimization.

--- lib/EightLetters.pm
+++ lib/EightLetters.pm
@@ -7,12 +7,9 @@ EightLetters - Perl module to calculate which eight letter word spells the most 
 =cut
 
 use integer;
-use FindBin;
 use Moo;
-use File::Slurp;
-use Config;
 use Inline C => 'DATA';
-use Inline C => Config => ccflags => $Config{'ccflags'} . ' -Ofast';
+use Inline C => Config => ccflagsex => '-Ofast';
 use Parallel::ForkManager;
 use Sys::Info;
 no warnings 'experimental::postderef';
@@ -20,99 +17,68 @@ use feature 'postderef';
 
 our $VERSION = '2.0';
 
-use constant {
-    DICTIONARY          => "$FindBin::Bin/../lib/dict/2of12inf.txt",
-    SIGT                => 0,
-    COUNT               => 1,
-    ZEROBV              => "\0" x (32*8), #do {my $bv; vec($bv, $_*32, 32) = 0 for 0 .. 7; $bv},
-    ORD_A               => ord 'a',
-    CORE_MULTIPLIER     => 3,       # In testing, 2 is better on an i5 with 4 cores,
-                                    # 3 is better on i7 with 4 cores, 8 logical.
-};
-
-has dict_path       => (is => 'ro', default => DICTIONARY);
-has dict            => (is => 'lazy');
+use constant CORE_MULTIPLIER => 3;  # In testing, 2 is better on an i5 with 4 cores,
+                                    # 3 seems better on i7 with 4 cores, 8 logical.
+has dict_path       => (is => 'ro',);
 has count           => (is => 'lazy');
 has letters         => (is => 'lazy');
 has buckets         => (is => 'rw', default => sub {{}});
 has words           => (is => 'rw', default => sub {{}});
 has _count_internal => (is => 'rw');
-has _pm             => (is => 'lazy');
-has _num_processes  => (is => 'lazy');
-
-# Skipping words with jkqvxz is obviously faster but makes unsafe assumptions for an arbitrary dict.
-sub _build_dict {[map {(m/^([a-z]{1,8})\b/ && $1) || ()} read_file($_[0]->dict_path)]}
 
 sub _build_count {
     $_[0]->letters;
     $_[0]->_count_internal;
 }
 
-sub _build_signature {
-    my($bv, @hist) = (ZEROBV, (0)x26);
-    $hist[ord() - ORD_A]++ for unpack '(A)*', $_[1];
-    for (0 .. $#hist) {
-        vec($bv, $hist[$_]*26+$_, 1) = 1 while $hist[$_]--;
-    }
-    [unpack 'Q4', $bv];
+sub _signature {
+        my($bv, @hist) = ("\0"x256, (0)x26); # Zero bit-vector, histograms.
+        $hist[ord() - 97]++ for unpack '(A)*', $_[0];  # ord('a') == 97.
+        for (0 .. $#hist) {
+            vec($bv, $hist[$_]*26+$_, 1) = 1 while $hist[$_]--;
+        }
+        [unpack 'Q4', $bv];
 }
-
-sub _build__num_processes {(Sys::Info->new->device('CPU')->count) * CORE_MULTIPLIER}
-sub _build__pm {Parallel::ForkManager->new(shift()->_num_processes)}
 
 sub _organize_words {
     my($b, $w) = ($_[0]->buckets, $_[0]->words);
-    for ($_[0]->dict->@*) {
-        my $letters = join '', sort unpack '(A)*';
-        my $ref = (8 == length) ? $b : $w;
-        $ref->{$letters} = [$_[0]->_build_signature($_), 0]
+    for (map {(m/^([a-z]{1,8})\b/ && $1)||()} do {open my $fh, '<', $_[0]->dict_path; <$fh>}) {
+        my ($letters, $ref) = (
+            join('', sort unpack '(A)*'),
+            (8 == length) ? $b : $w,
+        );
+        $ref->{$letters} = [_signature($_), 0]
             unless exists $ref->{$letters};
-        $ref->{$letters}[COUNT]++;
+        $ref->{$letters}[1]++;
     }
 }
 
 sub _build_letters {
     my $self = shift;
 
-    print "Organizing words.\n";
     $self->_organize_words;
-
-    my $np = $self->_num_processes;
-    my $mult = CORE_MULTIPLIER;
-    my $ncores = $np/$mult;
-
-    print "Tallying buckets with $np processes ($ncores cores * $mult).\n";
     $self->_increment_counts;
 
-    print "Finding biggest bucket.\n";
     my($bucket_name, $count) = $self->_count_buckets;
     $self->_count_internal($count);
     $bucket_name;
 }
 
-
 sub _increment_counts {
-    my $words = [values $_[0]->words->%*];
-
-    my $n = 0;
-    my $m = $_[0]->_num_processes;
-
-    my @batches;
-    my $buckets = $_[0]->buckets;
-    scalar keys %$buckets; # Reset the iterator.
+    my ($words, $n, $m, $buckets, @batches)
+        = ([values $_[0]->words->%*], 0, (Sys::Info->new->device('CPU')->count) * CORE_MULTIPLIER, $_[0]->buckets, ());
 
     while (my ($key, $v) = each %$buckets) {
         push @{$batches[$n++ % $m]}, [$key, $v];
     }
 
-    my $pm = $_[0]->_pm;
+    my $pm = Parallel::ForkManager->new($m);
+    $pm->set_waitpid_blocking_sleep(0);
     $pm->run_on_finish(sub {$buckets->{$_->[0]} = $_->[1] for $_[5]->@*});
 
     for my $batch (@batches) {
         $pm->start and next;
-
         $_[0]->_process_bucket($_->[1], $words) for @$batch;
-
         $pm->finish(0, $batch);
     }
     $pm->wait_all_children;
@@ -120,13 +86,11 @@ sub _increment_counts {
 
 sub _count_buckets {
     my($count, $buckets, $letters) = (0, $_[0]->buckets, '');
-    scalar keys $_[0]->buckets->%*; # Reset the iterator.
+
     while(my($bucket_key, $bucket) = each %$buckets) {
-        if($bucket->[COUNT] > $count) {
-            $letters = $bucket_key;
-            $count = $bucket->[COUNT];
-        }
+        ($letters, $count) = ($bucket_key, $bucket->[1]) if $bucket->[1] > $count;
     }
+
     ($letters, $count);
 }
 
@@ -140,9 +104,7 @@ __C__
 
 void _process_bucket( SV* self, SV* b, SV* words ) {
 
-    // Unpack the arguments.
     AV* b_av     = (AV*) SvRV(b);
-    SV* b_count  = (SV*) *(av_fetch(b_av,COUNT,0));
     AV* words_av = (AV*) SvRV(words);
     AV* bs_av    = (AV*) SvRV(*( av_fetch(b_av,SIGT,0)));
 
@@ -152,7 +114,7 @@ void _process_bucket( SV* self, SV* b, SV* words ) {
     bs[2] = ~SvIV(*(av_fetch(bs_av,2,0)));
     bs[3] = ~SvIV(*(av_fetch(bs_av,3,0)));
 
-    size_t ix=0;
+    size_t ix  = 0;
     size_t top = av_top_index(words_av);
     for( ; ix <= top; ++ix ) {
 
@@ -164,6 +126,7 @@ void _process_bucket( SV* self, SV* b, SV* words ) {
             && !(SvIV(*(av_fetch(ws_av,2,0))) & bs[2])
             && !(SvIV(*(av_fetch(ws_av,3,0))) & bs[3])
         ) {
+            SV* b_count = (SV*) *(av_fetch(b_av,COUNT,0));
             sv_setiv(b_count, SvIV(b_count) + SvIV(*(av_fetch(word_av,COUNT,0))));
         }
     }
